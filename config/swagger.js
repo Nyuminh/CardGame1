e368dcb9e349ac604cb06e25b0fee4f845793910
Minh Tâm <tamnmse184234@fpt.edu.vn>
2025-09-12 16:25:29 +0700

    test render

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'http://localhost:3000' || 'https://cardgame-main.onrender.com/',
         description: 'Development server'
       }
     ],
